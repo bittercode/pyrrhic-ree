51e4a98bf12396374197a88e67d53421337ad290
Barrett Lewis <musikal.fusion@gmail.com>
2013-02-23 23:36:42 -0800

    moved expression.py to controller.py
    More functionality added to controller.py
    
    The following is implemented:
    * a single Controller
    * built in embedded flag checker - returns set of flags
    * removed unecessary calls to unicode

--- ReeController/controller.py
+++ ReeController/controller.py
@@ -8,48 +8,54 @@ all views must be programmed against.
 
 import re
 
-class ree:
+# Using a class because it allows us to set up properties
+# which you can't do in the module level
+class _ree:
     """The Regex API."""
     def __init__(self):
         self._regex = ""
         self._matchstring = ""
         self._replaceString = ""
-        self._flags = 0
-        self._compiledRegex = re.compile(self._regex, self._flags)
+        self.flags = 0
+        self.compiledRegex = re.compile(self._regex, self.flags)
+        self._flagChecker = re.compile(r"^ *\(\?(?P<flags>[aiLmsx]*)\)")
 
+    #  use property to force regex compile on set
     def regex():
         doc = "The regex string"
         def fget(self):
             return self._regex
         def fset(self, value):
-            try:
-                self._regex = str(value)
-            except UnicodeError:
-                self._regex = unicode(value)
-            self._compiledRegex = re.compile(self._regex, self._flags)
+            self._regex = str(value)
+            self.compiledRegex = re.compile(self._regex, self.flags)
         return locals()
     regex = property(**regex())
 
+    #  use property to force new values to convert to strings
     def matchString():
         doc = "The match string."
         def fget(self):
             return self._matchString
         def fset(self, value):
-            try:
-                self._matchString = str(value)
-            except UnicodeError:
-                self._matchstring = unicode(value)
+            self._matchString = str(value)
         return locals()
     matchString = property(**matchString())
 
+    #  use property to force new values to convert to strings
     def replaceString():
         doc = "The replacement string"
         def fget(self):
             return self._replaceString
         def fset(self, value):
-            try:
-                self._replaceString = str(value)
-            except UnicodeError:
-                self._replaceString = unicode(value)
+            self._replaceString = str(value)
         return locals()
     replaceString = property(**replaceString())
+
+    def embeddedFlags(self):
+        match = self._flagChecker.match(self.regex)
+        return set(match.group('flags')) if match else set() 
+
+
+
+Controller = _ree()
+
